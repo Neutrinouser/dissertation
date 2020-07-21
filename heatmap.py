bb25bc0af5e74df8d00432a7cc5f5f75de2f3b8a
Georgios Aliatimis <aliatimis@maths.ox.ac.uk>
2020-07-21 23:25:01 +0100

    Made some stylistic changes in heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -3,21 +3,23 @@ import matplotlib.pyplot as plt
 import pickle
 
 with open('parameterSweepData.p','rb') as handle:
-    d = pickle.load(handle); 
-
-features = ['dispersion','frontPosition','tumourRadius']
+    d = pickle.load(handle) 
 
 # In scientific notation
-ylabels, y = zip(*[("{:.1e}".format(i),i) for i in d.keys() ])
+ylabels, y = zip(*[(round(i,1),i) for i in d.keys() ])
 xlabels, x = zip(*[("{:.1e}".format(i),i) for i in d[y[0]].keys()])
 
+# Name of features
+features = d[y[0]][x[0]].keys()
+
 # Form new dictionary
-#s = {k: [[j[k][0] for j in i.values() ] for i in d.values() ] for k in features}
-s = {k: [[ d[j][i][k][0] for i in x ] for j in y] for k in d[y[0]][x[0]].keys()}
+s = {k: [[ d[j][i][k][0] for i in x ] for j in y] for k in features}
 
 for k in features:
     ax=seaborn.heatmap(s[k])
     ax.set_xticklabels(xlabels)
     ax.set_yticklabels(ylabels)
     plt.title(k)
+    plt.xlabel('Diffusivity of microbeads')
+    plt.ylabel('Proliferation of cells')
     plt.show()
