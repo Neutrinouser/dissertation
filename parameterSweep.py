47d0c5e44fc36cd4e0f0cb7f975b666b9b92b8aa
George Aliatimis <galiatimis@gmail.com>
2020-07-21 21:03:13 +0300

    Deleted Features class in parameterSweep.py and used Solver.featureExtraction directly.

--- parameterSweep.py
+++ parameterSweep.py
@@ -4,38 +4,12 @@ import numpy as np
 import pickle
 import seaborn
 
-class Features:
-    def __init__(self,timeSet=[],**kwargs):
-        self.results = Solver.featureExtraction(**kwargs)
-        if not timeSet:
-            timeSet = self.results.keys()
-        self.outputDict = {}; 
-        k = np.array(list(self.results.keys()))
-        self.timeSet = [k[np.argmin(np.abs( k - time ))] for time in timeSet]; 
-
-    def outputDictionary(self):
-        values = [self.results[j] for j in self.timeSet]; 
-        features = values[0].keys(); 
-        self.outputDict['times'] = self.timeSet
-        for j in features:
-            self.outputDict[j] = [v[j] for v in values] 
-        return self.outputDict
-
-    def plotting(self,featureSet = None ):
-        if not featureSet:
-            featureSet = self.outputDict.keys(); 
-        _= self.outputDictionary(); 
-        for j in featureSet:
-            plt.plot(self.timeSet, self.outputDict[j])
-        plt.show()
-
-
-sList = np.linspace(7,15,10); 
-mu_mList = np.linspace(5e-3,5e-2, 10); 
+sList = np.linspace(7,13,3); 
+mu_mList = np.linspace(5e-3,5e-2, 3); 
 inputDictionary = dict(s=sList,mu_m=mu_mList); 
 
 
-def parameterSweep(inputs):
+def parameterSweep(inputs,timeSet=[1.0]):
     keys = list(inputs.keys())
     L = len(keys) - 1
     def iteration(input,l):
@@ -45,13 +19,13 @@ def parameterSweep(inputs):
             inputNew = input; 
             inputNew.update({key: i}) 
             if l==L:
-                inputNew.update(timeSet=[1.0])
-                out[i] = Features(**inputNew).outputDictionary()
+                inputNew.update(timeSet=timeSet)
+                out[i] = Solver.featureExtraction(**inputNew)
             else:
                 out[i] = iteration(inputNew,l+1)
         return out
     return iteration({},0)
 
 d = parameterSweep(inputDictionary); 
-with open('data.p','wb') as fp:
+with open('parameterSweepData.p','wb') as fp:
     pickle.dump(d,fp)
